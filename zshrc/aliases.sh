0ff2eb51759ffd77e18905b7b8079deea0ae5518
MathurinMoquin <mathurinmoquin1@gmail.com>
2025-09-16 21:30:49 -0400

    add hypr nvim waybar wofi zshrc/bashrc configs

---
+++ zshrc/aliases.sh
@@ -0,0 +1,39 @@
+#!/bin/zsh
+
+# Alias
+
+alias cl='clear'
+alias nv='nvim'
+alias v='vim'
+alias py='python3'
+alias b='sudo ~/.script/startBluetooth'
+#alias b='sudo service bluetooth start'
+alias code='code-oss'
+alias runr='cargo run'
+alias up='sudo apt update && sudo apt upgrade'
+alias e='exit'
+alias rustr='cargo build --release'
+alias lla='ls -la'
+alias jeu='wine ~/Documents/EchoesOfTheFallen/Echoes\ of\ the\ Fallen.exe'
+alias sail='./vendor/bin/sail'
+alias vimz='vim ~/.zshrc'
+alias bn='sudo ~/.script/brightness'
+
+# Hyprland
+
+alias sb='source ~/.bashrc'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+alias hw='hyprctl clients -j | jq'
+#alias vim='nvim'
+
+# Projets
+alias nd='cd $HOME/Programs/School/Multiplateforme/notdiscordAll'
+alias m2='cd $HOME/Programs/School/Mobile2/exo1pp/src'
+alias ps='cd $HOME/Programs/School'
+
+# Object C
+alias idf='. $HOME/esp/esp-idf/export.sh'
---
+++ zshrc/exports.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ANDROID_HOME=~/Android/Sdk
+export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+
+export HYPRSHOT_DIR=$HOME/Pictures/screenshots
